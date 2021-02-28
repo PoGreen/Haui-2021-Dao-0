259da9b35bc2a270cf5e10e0bc60eb20eb9df7d6
pogreen <trandaogrey@gmail.com>
2021-02-28 15:03:28 +0700

    update add template

--- src/main/java/haui/estate/business/controllers/HomeController.java
+++ src/main/java/haui/estate/business/controllers/HomeController.java
@@ -2,11 +2,7 @@ package haui.estate.business.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @Controller
-@RequestMapping(value = "/home")
 public class HomeController {
 
     @GetMapping
